0ee8c7afa7dbf1ed4ab01f608647b0a8acbdbc33
Krzysiek526 <filipkrzys526@gmail.com>
2020-07-29 11:03:18 +0200

    fixing database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-reticulated-02565',
-        user: 'postgres',
-        password: 'krzysiek',
-        database: 'faceapp'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 })
 
